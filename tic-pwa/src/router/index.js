5161590b787fc74f0f5fe8619334d8360287a05c
Jonas Mikea <jonamikeafilho00@gmail.com>
2025-11-03 19:16:42 -0300

    Corrige imports do router com extensão .js para o build

--- tic-pwa/src/router/index.js
+++ tic-pwa/src/router/index.js
@@ -1,7 +1,7 @@
 // tic-pwa/src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import { useAuthStore } from '@/stores/auth.store';
+import { useAuthStore } from '@/stores/auth.store.js';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
